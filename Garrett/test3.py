41ba85efa609343f76eceb4654a24385e2dabf1a
Garrett Cole <garbear509@gmail.com>
2023-04-11 12:19:48 -0400

    Update
    Added more functionality:
    - can now remove tasks
    - can now save data

--- Garrett/test3.py
+++ Garrett/test3.py
@@ -1,9 +1,12 @@
 import sqlite3
-import sys
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem, QMessageBox
-from PyQt5 import uic
-from PyQt5 import QtCore, QtGui
+from PyQt5 import uic, QtGui
+import sys
+
+from PyQt5 import QtCore
+
 
 class Window(QWidget):
     def __init__(self):
@@ -15,18 +18,20 @@ class Window(QWidget):
         self.addButton.clicked.connect(self.addNewTask)
 
     def calendarDateChanged(self):
+        print("The calendar date was changed.")
         dateSelected = self.calendarWidget.selectedDate().toPyDate()
+        print("Date selected:", dateSelected)
         self.updateTaskList(dateSelected)
 
     def updateTaskList(self, date):
         self.tasksListWidget.clear()
-        db = sqlite3.connect('Garrett\data.db')
+
+        db = sqlite3.connect('Garrett/data.db')
         cursor = db.cursor()
 
         query = "SELECT task, completed FROM tasks WHERE date = ?"
         row = (date,)
         results = cursor.execute(query, row).fetchall()
-
         for result in results:
             item = QListWidgetItem(str(result[0]))
             item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
@@ -39,7 +44,7 @@ class Window(QWidget):
         self.highlightDaysWithTasks()
 
     def saveChanges(self):
-        db = sqlite3.connect("Garrett\data.db")
+        db = sqlite3.connect("Garrett/data.db")
         cursor = db.cursor()
         date = self.calendarWidget.selectedDate().toPyDate()
 
@@ -54,13 +59,21 @@ class Window(QWidget):
             cursor.execute(query, row)
         db.commit()
 
+        # Remove checked items from the list
+        for i in reversed(range(self.tasksListWidget.count())):
+            item = self.tasksListWidget.item(i)
+            if item.checkState() == QtCore.Qt.Checked:
+                self.tasksListWidget.takeItem(i)
+
         messageBox = QMessageBox()
         messageBox.setText("Changes saved.")
         messageBox.setStandardButtons(QMessageBox.Ok)
         messageBox.exec()
 
+        self.highlightDaysWithTasks()
+
     def addNewTask(self):
-        db = sqlite3.connect("Garrett\data.db")
+        db = sqlite3.connect("Garrett/data.db")
         cursor = db.cursor()
 
         newTask = str(self.taskLineEdit.text())
@@ -72,19 +85,39 @@ class Window(QWidget):
         db.commit()
         self.updateTaskList(date)
         self.taskLineEdit.clear()
+        self.highlightDaysWithTasks()
 
-def highlightDaysWithTasks():
-    db = sqlite3.connect('Garrett\data.db')
-    cursor = db.cursor()
-    query = "SELECT DISTINCT date FROM tasks"
-    results = cursor.execute(query).fetchall()
+    def highlightDaysWithTasks(self):
+        db = sqlite3.connect("Garrett\data.db")
+        cursor = db.cursor()
+        query = "SELECT DISTINCT date FROM tasks"
+        results = cursor.execute(query).fetchall()
 
-    datesWithTasks = [QtCore.QDate(*x) for x in results]
+        datesWithTasks = [QtCore.QDate(*x) for x in results]
 
-    for date in datesWithTasks:
-        calDate = QtWidgets.QCalendarWidget().selectedDate()
-        if date == calDate:
-            bgBrush = QtGui.QBrush(QtGui.QColor(0, 255, 0, 100))
-            fgBrush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-            fmt = QtGui.QTextCharFormat()
-            fmt.setBackground
+        # get the current month and year of the calendar
+        currentMonth = self.calendarWidget.monthShown()
+        currentYear = self.calendarWidget.yearShown()
+
+        # iterate through the days in the current month
+        for day in range(1, 32):
+            try:
+                # create a date object for the current day
+                date = QtCore.QDate(currentYear, currentMonth, day)
+
+                # set the background color of the day if there are tasks on that day
+                if date in datesWithTasks:
+                    bgBrush = QtGui.QBrush(QtGui.QColor(0, 255, 0, 100))
+                    fmt = QtGui.QTextCharFormat()
+                    fmt.setBackground(bgBrush)
+                    self.calendarWidget.setDateTextFormat(date, fmt)
+
+            except ValueError:
+                # if the day is invalid for the current month, move on to the next day
+                continue
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
